7b836168b4f50a74fab3ed05c7f304da7b110af1
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-08-09 20:25:27 -0300

    Classe com métodos de ordenação para ArrayList<Integer>

--- src/javax/util/arraylist/ArrayListInteger.java
+++ src/javax/util/arraylist/ArrayListInteger.java
@@ -5,8 +5,14 @@ import java.util.ArrayList;
 import javax.util.ArrayListInterface;
 
 /**
+ * A classe ArrayListInteger é uma subclasse de ArrayList<Integer> e ela
+ * implementa métodos de ordenação para a instância de ArrayListInteger.
+ * Os métodos utilizam quick sort para ordenação e podem ser ordenados
+ * tanto de forma ascendente como de forma descendente.
+ * 
  * @author Pedro Marcelo de Sá Alves
  * @since August 9, 2013
+ * @see ArrayList
  */
 public final class ArrayListInteger extends ArrayList<Integer> implements
 		ArrayListInterface<Integer> {
